2a8e7f09794fb0182b38dac43e9280522880567e
Kyle Abens <25699262+kyleabens@users.noreply.github.com>
2024-01-10 14:06:25 -0500

    Update PhoneAuthProviderHandler.swift

--- packages/authentication/ios/Plugin/Handlers/PhoneAuthProviderHandler.swift
+++ packages/authentication/ios/Plugin/Handlers/PhoneAuthProviderHandler.swift
@@ -13,10 +13,60 @@ class PhoneAuthProviderHandler: NSObject {
         super.init()
     }
 
-    func signIn(_ options: SignInWithPhoneNumberOptions) {
-        signInOnConfirm = true
-        skipNativeAuthOnConfirm = options.getSkipNativeAuth()
-        verifyPhoneNumber(options)
+    func signIn(call: CAPPluginCall) {
+        let phoneNumber = call.getString("phoneNumber");
+        
+        if phoneNumber == nil {
+            let email = call.getString("email")!;
+            let password = call.getString("password")!;
+            
+            Auth.auth().signIn(withEmail: email,
+                               password: password) { (result, error) in
+              let authError = error as NSError?
+              if (authError == nil || authError!.code != AuthErrorCode.secondFactorRequired.rawValue) {
+                // User is not enrolled with a second factor and is successfully signed in.
+                // ...
+              } else {
+                let resolver = authError!.userInfo[AuthErrorUserInfoMultiFactorResolverKey] as! MultiFactorResolver
+                // Ask user which second factor to use.
+                let hint = resolver.hints[0] as! PhoneMultiFactorInfo
+                // Send SMS verification code
+                PhoneAuthProvider.provider().verifyPhoneNumber(
+                  with: hint,
+                  uiDelegate: nil,
+                  multiFactorSession: resolver.session) { (verificationId, error) in
+                      if let error = error {
+                          self.pluginImplementation.handleFailedSignIn(message: nil, error: error)
+                          return
+                      }
+                      
+                      var result = FirebaseAuthenticationHelper.createSignInResult(credential: nil, user: nil, idToken: nil, nonce: nil, accessToken: nil, serverAuthCode: nil, additionalUserInfo: nil, displayName: nil, authorizationCode: nil)
+                      result["verificationId"] = verificationId
+                      call.resolve(result)
+                }
+              }
+            }
+            
+        } else {
+            
+            guard let phoneNumber = phoneNumber else {
+                return
+            }
+            PhoneAuthProvider.provider()
+                .verifyPhoneNumber(phoneNumber, uiDelegate: nil) { verificationID, error in
+                    if let error = error {
+                        self.pluginImplementation.handleFailedSignIn(message: nil, error: error)
+                        return
+                    }
+
+                    var result = FirebaseAuthenticationHelper.createSignInResult(credential: nil, user: nil, idToken: nil, nonce: nil, accessToken: nil, serverAuthCode: nil, additionalUserInfo: nil, displayName: nil, authorizationCode: nil)
+                    result["verificationId"] = verificationID
+                    call.resolve(result)
+                }
+            
+        }
+        
+    
     }
 
     func link(_ options: LinkWithPhoneNumberOptions) {
@@ -39,13 +89,8 @@ class PhoneAuthProviderHandler: NSObject {
 
     private func verifyPhoneNumber(_ options: SignInWithPhoneNumberOptions) {
         PhoneAuthProvider.provider()
-            .verifyPhoneNumber(options.getPhoneNumber(), uiDelegate: nil) { verificationID, error in
-                if let error = error {
-                    self.pluginImplementation.handlePhoneVerificationFailed(error)
-                } else {
-                    self.pluginImplementation.handlePhoneCodeSent(verificationID ?? "")
-                }
+            .verifyPhoneNumber(options.getPhoneNumber(), uiDelegate: nil) { verificationID, _ in
+                self.pluginImplementation.handlePhoneCodeSent(verificationID ?? "")
             }
     }
-
 }
